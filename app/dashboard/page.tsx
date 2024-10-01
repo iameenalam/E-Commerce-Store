64117a53f5ac696eb6743eec6436874ac601ea6a
Ameen Alam~ <ameenalam98@gmail.com>
2024-10-01 11:12:51 +0500

    fetched data from the chart in dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,3 @@
-import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import {
   Card,
   CardContent,
@@ -8,20 +7,54 @@ import {
 } from "@/components/ui/card";
 import { DashboardStats } from "../components/dashboard/DashboardStats";
 import { RecentSales } from "../components/dashboard/RecentSales";
+import { Chart } from "../components/dashboard/Chart";
+import prisma from "../lib/db";
 
-export default function Dashboard() {
+async function getData() {
+  const now = new Date();
+  const sevenDaysAgo = new Date();
+  sevenDaysAgo.setDate(now.getDate() - 7);
+
+  const data = await prisma.order.findMany({
+    where: {
+      createdAt: {
+        gte: sevenDaysAgo,
+      },
+    },
+    select: {
+      amount: true,
+      createdAt: true,
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+  });
+
+  const result = data.map((item) => ({
+    date: new Intl.DateTimeFormat("en-US").format(item.createdAt),
+    revenue: item.amount / 100,
+  }));
+
+  return result;
+}
+
+export default async function Dashboard() {
+  const data = await getData();
   return (
     <>
       <DashboardStats />
 
-      <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3 mt-10">
+      <div className="grid gap-4 md:gp-8 lg:grid-cols-2 xl:grid-cols-3 mt-10">
         <Card className="xl:col-span-2">
           <CardHeader>
             <CardTitle>Transactions</CardTitle>
             <CardDescription>
-              Recent transactions from your store
+              Recent transactions from the last 7 days
             </CardDescription>
           </CardHeader>
+          <CardContent>
+            <Chart data={data} />
+          </CardContent>
         </Card>
 
         <RecentSales />
