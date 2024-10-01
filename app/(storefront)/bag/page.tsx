54ffa21676a6fe70b68a13e533802eed6c91aef5
Ameen Alam~ <ameenalam98@gmail.com>
2024-10-01 11:54:35 +0500

    finished caching

--- app/(storefront)/bag/page.tsx
+++ app/(storefront)/bag/page.tsx
@@ -7,10 +7,11 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { ShoppingBag } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-
 import { redirect } from "next/navigation";
+import { unstable_noStore as noStore } from "next/cache";
 
 export default async function BagRoute() {
+  noStore();
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -1,8 +1,10 @@
 import prisma from "@/app/lib/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextResponse } from "next/server";
+import { unstable_noStore as noStore } from "next/cache";
 
 export async function GET() {
+  noStore();
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
@@ -29,5 +31,9 @@ export async function GET() {
     });
   }
 
-  return NextResponse.redirect("http://localhost:3000/");
+  return NextResponse.redirect(
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/"
+      : "https://shoe-marshal.vercel.app/"
+  );
 }
